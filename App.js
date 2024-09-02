289624bada7e3ef4b0edc614807e5c1eb28f6100
o7lipy00 <o7lipy00@students.oamk.fi>
2024-09-02 20:16:25 +0300

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Modal, Pressable, BackHandler }
+ from 'react-native';
 
 export default function App() {
+  const[modalVisible, setModalVisible] = useState(false);// TILAMUUTTUJA JOKA ON ALUKSI
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        style={styles.button}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.buttonText}>Show Modal</Text>
+      </Pressable>
+
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        animationType="slide"
+       
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>This is modal</Text>
+            <Pressable
+              style={[styles.button, styles.closeButton]}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.buttonText}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  modalContent: {
+    backgroundColor: 'grey',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'center',
+    height: 200,
+  },
+  closeButton: {
+    marginTop: 80, 
   },
 });
